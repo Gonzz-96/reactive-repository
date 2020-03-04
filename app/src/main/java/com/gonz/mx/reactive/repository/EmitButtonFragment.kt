eb05ec59f21a1f3b78244b3b41480c2517f66dd5
Gonzalo Campos <erick.campos@wizeline.com>
2020-03-03 20:19:49 -0600

    Implementing observing logic

--- app/src/main/java/com/gonz/mx/reactive/repository/EmitButtonFragment.kt
+++ app/src/main/java/com/gonz/mx/reactive/repository/EmitButtonFragment.kt
@@ -5,12 +5,24 @@ import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import org.koin.android.ext.android.inject
 
 class EmitButtonFragment : Fragment(R.layout.fragment_emit_button) {
 
+    private val emitButtonViewModel: EmitButtonViewModel by inject()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         view.findViewById<Button>(R.id.btn_emit).run {
-            setOnClickListener { Toast.makeText(requireContext(), "Pressed", Toast.LENGTH_SHORT).show() }
+            setOnClickListener {
+                emitButtonViewModel.incrementCounter()
+            }
         }
+
+        emitButtonViewModel.value.observe(viewLifecycleOwner, Observer {
+            Toast.makeText(requireContext(), "Value changed: $it!", Toast.LENGTH_SHORT).show()
+        })
+
+        emitButtonViewModel.observeData()
     }
 }
