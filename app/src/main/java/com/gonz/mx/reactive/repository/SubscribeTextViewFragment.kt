9fbcbc5bdde235a4f80cadb85bafced483e4c1e7
Gonzalo Campos <erick.campos@wizeline.com>
2020-03-03 20:19:23 -0600

    Injecting viewmodel

--- app/src/main/java/com/gonz/mx/reactive/repository/SubscribeTextViewFragment.kt
+++ app/src/main/java/com/gonz/mx/reactive/repository/SubscribeTextViewFragment.kt
@@ -1,5 +1,19 @@
 package com.gonz.mx.reactive.repository
 
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import org.koin.android.ext.android.inject
 
-class SubscribeTextViewFragment : Fragment(R.layout.fragment_subscribe_text_view)
+class SubscribeTextViewFragment : Fragment(R.layout.fragment_subscribe_text_view) {
+
+    private val viewModel: SubscribeTextViewViewModel by inject()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        viewModel.value.observe(viewLifecycleOwner, Observer {
+            view.findViewById<TextView>(R.id.tv_observer).text = "$it"
+        })
+    }
+}
