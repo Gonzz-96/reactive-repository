24ad99ff7852cc8922253de9bde9a7bab2378c4b
Gonzalo Campos <erick.campos@wizeline.com>
2020-03-03 19:48:26 -0600

    Adding EmitFragment to main activity

--- app/src/main/java/com/gonz/mx/reactive/repository/EmitButtonFragment.kt
+++ app/src/main/java/com/gonz/mx/reactive/repository/EmitButtonFragment.kt
@@ -1,7 +1,16 @@
 package com.gonz.mx.reactive.repository
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 
 class EmitButtonFragment : Fragment(R.layout.fragment_emit_button) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        view.findViewById<Button>(R.id.btn_emit).run {
+            setOnClickListener { Toast.makeText(requireContext(), "Pressed", Toast.LENGTH_SHORT).show() }
+        }
+    }
 }
--- app/src/main/java/com/gonz/mx/reactive/repository/MainActivity.kt
+++ app/src/main/java/com/gonz/mx/reactive/repository/MainActivity.kt
@@ -2,11 +2,21 @@ package com.gonz.mx.reactive.repository
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.IdRes
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        addFragment(R.id.fragment_emit_button, EmitButtonFragment())
+    }
+
+    private fun addFragment(@IdRes container: Int, fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .add(container, fragment)
+            .commit()
     }
 }
